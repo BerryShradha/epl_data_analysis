f1140bbe1ce3dc379b917936636091a0ee9e2605
shradha berry <shradhaberry@gmail.com>
2019-03-29 23:37:55 +0000

    Data Preparation completed

--- scripts/Data_Preparation.R
+++ scripts/Data_Preparation.R
@@ -4,9 +4,10 @@
 # 1. Read epl.csv, weather.csv and dates_location_mapping.xlsx
 # 2. Sync data types of joining attributes
 # 3. Join datasets
-# 4. Cleanup the merged dataset. Check for NAs, duplicates, outliers
+# 4. Cleanup the merged dataset. Check for NAs, duplicates
 # 5. Perform feature selection
-# 6. Perform exploratory analysis
+# 6. Analyse outliers
+# 7. Perform exploratory analysis
 
 #######################################################################
 
@@ -73,70 +74,76 @@ merged_data_na #Shows 19 weather records are consistently NA.
 #Handle at individual columns that have missing data
 colnames(merged_data)[apply(merged_data, 2, anyNA)]
 
-## a. daily.pressure
-sum(is.na(merged_data$daily.pressure)) #2 NAs
+## a. daily.precipAccumulation
+sum(is.na(merged_data$daily.precipAccumulation)) #3375 NAs
+#Almost all rows have NA. Remove this attribute 
+merged_data_clean <- within(merged_data, rm(daily.precipAccumulation))
+str(merged_data_clean)
 
-## b. daily.cloudcover
-sum(is.na(merged_data$daily.cloudCover)) #41 NAs
+## b. daily.pressure
+sum(is.na(merged_data_clean$daily.pressure)) #2 NAs
 
-## c. daily.visibility
-sum(is.na(merged_data$daily.visibility)) #23 NAs
+## c. daily.cloudcover
+sum(is.na(merged_data_clean$daily.cloudCover)) #41 NAs
+
+## d. daily.visibility
+sum(is.na(merged_data_clean$daily.visibility)) #23 NAs
 
 #Replace NAs for pressure,cloudcover,visibility with corresponding value at nearest coordinate on that day, if daily.summary is same
 #install.packages("stringdist")
 library(stringdist)
-replace_na <- function(merged_data) {
-  for (i in 1:dim(merged_data)[1]) {
-    if (is.na(merged_data$daily.cloudCover[i]) | 
-        is.na(merged_data$daily.visibility[i]) | 
-        is.na(merged_data$daily.pressure[i])) {
-      records <- merged_data[merged_data$Date == merged_data$Date[i] & 
-                               merged_data$daily.icon == merged_data$daily.icon[i] & 
-                               merged_data$home.team != merged_data$home.team[i],]
+replace_na <- function(data) {
+  for (i in 1:dim(data)[1]) {
+    if (is.na(data$daily.cloudCover[i]) | 
+        is.na(data$daily.visibility[i]) | 
+        is.na(data$daily.pressure[i])) {
+      records <- data[data$Date == data$Date[i] & 
+                        data$daily.icon == data$daily.icon[i] & 
+                        data$home.team != data$home.team[i],]
       if (nrow(records) > 0)
-        record <- records[which.min(Reduce('+',Map(stringdist,records$Coordinates, merged_data$Coordinates[i], method='jaccard'))),]
+        record <- records[which.min(Reduce('+',Map(stringdist,records$Coordinates, data$Coordinates[i], method='jaccard'))),]
       if (nrow(record) > 0) {
-        if (is.na(merged_data$daily.cloudCover[i]) & !is.na(record$daily.cloudCover))
-          merged_data$daily.cloudCover[i] = record$daily.cloudCover
-        if (is.na(merged_data$daily.visibility[i]) & !is.na(record$daily.visibility))
-          merged_data$daily.visibility[i] <- record$daily.visibility
-        if (is.na(merged_data$daily.pressure[i]) & !is.na(record$daily.pressure))
-          merged_data$daily.pressure[i] <- record$daily.pressure
+        if (is.na(data$daily.cloudCover[i]) & !is.na(record$daily.cloudCover))
+          data$daily.cloudCover[i] = record$daily.cloudCover
+        if (is.na(data$daily.visibility[i]) & !is.na(record$daily.visibility))
+          data$daily.visibility[i] <- record$daily.visibility
+        if (is.na(data$daily.pressure[i]) & !is.na(record$daily.pressure))
+          data$daily.pressure[i] <- record$daily.pressure
       }
     }
   }
-  return(merged_data)
+  return(data)
 }
 
-merged_data_clean <- replace_na(merged_data)
+merged_data_clean <- replace_na(merged_data_clean)
 merged_data_na <- apply(is.na(merged_data_clean), 2, sum) #Check which attributes have NAs. 
 merged_data_na 
 
 #Still 2, 14, 14 records are NA. This is because there are no other match records on this day that have matching criteria
 #To handle these NAs, take an average of the attribute for the location, with same daily.icon
 library(dplyr)
-replace_na_v2 <- function(merged_data_clean) {
-  for (i in 1:dim(merged_data_clean)[1]) {
-    if (is.na(merged_data_clean$daily.cloudCover[i]) | 
-        is.na(merged_data_clean$daily.visibility[i]) | 
-        is.na(merged_data_clean$daily.pressure[i])) {
-      avgs <- merged_data_clean %>% 
-        filter(daily.icon == merged_data_clean$daily.icon[i], 
-               home.team == merged_data_clean$home.team[i], 
-               Date != merged_data_clean$Date[i]) %>%
+replace_na_v2 <- function(data) {
+  for (i in 1:dim(data)[1]) {
+    if (is.na(data$daily.cloudCover[i]) | 
+        is.na(data$daily.visibility[i]) | 
+        is.na(data$daily.pressure[i])) {
+      avgs <- data %>% 
+        filter(daily.icon == data$daily.icon[i], 
+               home.team == data$home.team[i], 
+               Date != data$Date[i]) %>%
         select(daily.cloudCover, daily.visibility, daily.pressure) %>% 
         summarize(avg_cloudcover = mean(daily.cloudCover, na.rm = TRUE), 
                   avg_visiblity = mean(daily.visibility, na.rm = TRUE), 
                   avg_pressure = mean(daily.pressure, na.rm = TRUE))
-      if (is.na(merged_data_clean$daily.cloudCover[i]))
-        merged_data_clean$daily.cloudCover[i] <- avgs$avg_cloudcover
-      if (is.na(merged_data_clean$daily.visibility[i]))
-        merged_data_clean$daily.visibility[i] <- avgs$avg_visiblity
-      if (is.na(merged_data_clean$daily.pressure[i]))
-        merged_data_clean$daily.pressure[i] <- avgs$avg_pressure
+      if (is.na(data$daily.cloudCover[i]))
+        data$daily.cloudCover[i] <- avgs$avg_cloudcover
+      if (is.na(data$daily.visibility[i]))
+        data$daily.visibility[i] <- avgs$avg_visiblity
+      if (is.na(data$daily.pressure[i]))
+        data$daily.pressure[i] <- avgs$avg_pressure
       }
     }
-  return(merged_data_clean)
+  return(data)
 }
 
 merged_data_clean <- replace_na_v2(merged_data_clean)
@@ -146,15 +153,8 @@ merged_data_na
 #Remove the two row that have NA in cloudcover and visibility
 colnames(merged_data_clean)[apply(merged_data_clean, 2, anyNA)]
 merged_data_clean <- merged_data_clean[complete.cases(merged_data_clean), ]
-merged_data_na <- apply(is.na(merged_data_clean), 2, sum) #Check which attributes have NAs. cloudcover 8, visibility 2
+merged_data_na <- apply(is.na(merged_data_clean), 2, sum) #Check which attributes have NAs. 
 merged_data_na 
-
-## d. daily.precipAccumulation
-sum(is.na(merged_data$daily.precipAccumulation)) #3375 NAs
-#Almost all rows have NA. Remove this attribute 
-merged_data_clean <- within(merged_data_clean, rm(daily.precipAccumulation))
-str(merged_data_clean)
-
 
 # 5. Perform feature selection (removing attributes) and feature extraction (deriving new attributes)
 str(merged_data_clean)
@@ -163,22 +163,89 @@ str(merged_data_clean)
 #daily.apparentTemperatureLowTime, daily.windGustTime, daily.uvIndexTime, daily.temperatureMinTime, daily.temperatureMaxTime,
 #daily.apparentTemperatureMinTime, daily.apparentTemperatureMaxTime, location, home.team
 #drop columns using dplyr function drop()
-final_data <- select (merged_data_clean,-c(Referee, FTHG, FTAG, Id, Club, Location, Coordinates, Latitude, Longitude, 
-                                           daily.sunriseTime, daily.sunsetTime, daily.precipIntensityMaxTime, 
-                                           daily.temperatureHighTime, daily.temperatureLowTime,id, 
-                                           daily.apparentTemperatureHighTime,daily.apparentTemperatureLowTime, 
-                                           daily.windGustTime, daily.uvIndexTime, daily.temperatureMinTime, 
-                                           daily.temperatureMaxTime,daily.apparentTemperatureMinTime, 
-                                           daily.apparentTemperatureMaxTime, location, home.team))
+final_data <- select (merged_data_clean,-c(Referee, FTHG, FTAG, Id, Club, Coordinates, Latitude, Longitude, 
+                                      daily.sunriseTime, daily.sunsetTime, daily.precipIntensityMaxTime, 
+                                      daily.temperatureHighTime, daily.temperatureLowTime,id, 
+                                      daily.apparentTemperatureHighTime,daily.apparentTemperatureLowTime, 
+                                      daily.windGustTime, daily.uvIndexTime, daily.temperatureMinTime, 
+                                      daily.temperatureMaxTime,daily.apparentTemperatureMinTime, 
+                                      daily.apparentTemperatureMaxTime, location, home.team))
 str(final_data)
+# 6. Analyse outliers
+#install.packages("qpcR")
+# function to check outliers for all attributes
+outliers <- function(data){
+  numeric_data <- select_if(data, is.numeric)
+  result <- NULL
+  for(i in 1:length(numeric_data)){
+    out <- boxplot.stats(eval(parse(text = paste("data$",names(numeric_data)[i]))))$out
+    result <- qpcR:::cbind.na(result, as.data.frame(out))
+    names(result)[i] <- names(numeric_data)[i]
+  }
+  return (result)
+}
 
+all_outliers <- outliers(final_data)
+all_outliers
 
-#Outlier detection
+#How many outliers
+apply(!is.na(all_outliers), 2, sum)
 
+#outlier values
+table(all_outliers$HTAG)
+boxplot(final_data$HTAG)
 
-# 6. Perform exploratory analysis
-str(merged_data_clean)
-boxplot(merged_data_clean$HTHG)$out
-hist(merged_data_clean$HTHG)
-boxplot(merged_data_clean$HTAG)$out
-hist(merged_data_clean$HTAG)
+# 7. Perform exploratory analysis
+library(ggplot2)
+library(gridExtra)
+
+# Same day weather difference between locations
+par(mar = c(6.5, 6.5, 0.5, 0.5), mgp = c(5, 1, 0))
+format(as.Date(final_data$Date,'%Y-%m-%d'), '%Y')
+format(tmp,'%Y')
+#Specific clubs, 2018
+ggplot(filter(final_data, HomeTeam %in% c("Chelsea", "Liverpool", "Man United") & format(as.Date(Date,'%Y-%m-%d'), '%Y') == 2018), 
+       aes(x = Date, y = daily.temperatureMax, group = HomeTeam)) + 
+  geom_line(aes(color = HomeTeam), size = 1) +
+  theme_minimal()  + xlab("Date") + ylab("Maximum Temperature for the Day")
+#All locations, 2018
+ggplot(filter(final_data, format(as.Date(Date,'%Y-%m-%d'), '%Y') == 2018), aes(x = Date, y = daily.temperatureMax, group = HomeTeam)) + 
+  geom_line(aes(color = Location), size = 1) +
+  theme_minimal()  + xlab("Date") + ylab("Maximum Temperature for the Day")
+
+# Same team home and away form
+opar <- par()
+par(mfrow = c(2,2))
+plot(filter(final_data, HomeTeam =="Chelsea")$HTR)
+plot(filter(final_data, AwayTeam == "Chelsea")$HTR)
+plot(filter(final_data, HomeTeam =="Chelsea")$FTR)
+plot(filter(final_data, AwayTeam == "Chelsea")$FTR)
+par(opar)
+
+#Label these properly. Left graphs are hometeam, right graphs are awayteam
+bar1 <- ggplot(filter(final_data, HomeTeam =="Chelsea"), aes(x = HTR)) + geom_bar()
+bar2 <- ggplot(filter(final_data, AwayTeam =="Chelsea"), aes(x = HTR)) + geom_bar()
+bar3 <- ggplot(filter(final_data, HomeTeam =="Chelsea"), aes(x = FTR)) + geom_bar()
+bar4 <- ggplot(filter(final_data, AwayTeam =="Chelsea"), aes(x = FTR)) + geom_bar()
+
+grid.arrange(bar1, bar2, bar3, bar4, ncol=2)
+
+#Same team form over time -------------Need to fix this. Need to consider away matches.
+ggplot(filter(final_data, HomeTeam %in% c("Chelsea", "Liverpool", "Man United") & format(as.Date(Date,'%Y-%m-%d'), '%Y') == 2018), 
+       aes(x = Date, y = as.factor(FTR), group = HomeTeam)) + 
+  geom_line(aes(color = HomeTeam), size = 1) +
+  theme_minimal()  + xlab("Date") + ylab("Maximum Temperature for the Day")
+
+# Distribution of goals
+hist1 <- ggplot(final_data, aes(x=HTHG)) + geom_histogram() + 
+  geom_histogram(color="black", fill="white")
+hist2 <- ggplot(final_data, aes(x=HTAG)) + geom_histogram() + 
+  geom_histogram(color="black", fill="white")
+grid.arrange(hist1, hist2, ncol=2)
+
+plot(final_data$HTR)
+plot(final_data$FTR)
+
+scatter <- ggplot(final_data, aes(HTHG, HTAG, colour=FTR)) + geom_point()
+scatter + geom_abline()
+scatter
--- scripts/Unsupervised.R
+++ scripts/Unsupervised.R
@@ -1,7 +1,34 @@
 #######################################################################
 ######################## UNSUPERVISED LEARNING ########################
 
-# 1. Cluster analysis
-# 2. PCA
+# 1. Correlation
+# 2. Cluster analysis
+# 3. PCA
 
 #######################################################################
+
+## 1. Correlation
+install.packages("tidyverse")
+library(tidyverse)
+# create label vectors for numeric weather attributes and numeric epl attributes
+weather_num <- names(select_if(final_data %>% dplyr:: select(starts_with("daily")), is.numeric))
+epl_num <- names(select_if(final_data %>% dplyr:: select(-starts_with("daily")), is.numeric))
+
+# exploring relationships among features: correlation matrix
+cor_weather_num <- cor(final_data[weather_num])
+cor_epl_num <- cor(final_data[epl_num])
+
+# visualize the correlation matrix
+cor_weather_num
+cor_epl_num
+
+# plot the relationships among features - scatterplot matrix
+pairs(final_data[weather_num])
+pairs(final_data[epl_num])
+
+
+# plot a more informative scatterplot matrix
+psych::pairs.panels(final_data[weather_num])
+psych::pairs.panels(final_data[epl_num])
+
+
