ebcab4570331e705065c56ce48b73041b8d30fa8
shradha berry <shradhaberry@gmail.com>
2019-03-29 12:19:47 +0000

    Handle NAs

--- scripts/Data_Preparation.R
+++ scripts/Data_Preparation.R
@@ -73,13 +73,13 @@ merged_data_na #Shows 19 weather records are consistently NA.
 #Handle at individual columns that have missing data
 colnames(merged_data)[apply(merged_data, 2, anyNA)]
 
-## 1. daily.pressure
+## a. daily.pressure
 sum(is.na(merged_data$daily.pressure)) #2 NAs
 
-## 2. daily.cloudcover
+## b. daily.cloudcover
 sum(is.na(merged_data$daily.cloudCover)) #41 NAs
 
-## 3. daily.visibility
+## c. daily.visibility
 sum(is.na(merged_data$daily.visibility)) #23 NAs
 
 #Replace NAs for pressure,cloudcover,visibility with corresponding value at nearest coordinate on that day, if daily.summary is same
@@ -87,7 +87,9 @@ sum(is.na(merged_data$daily.visibility)) #23 NAs
 library(stringdist)
 replace_na <- function(merged_data) {
   for (i in 1:dim(merged_data)[1]) {
-    if (is.na(merged_data$daily.cloudCover[i]) | is.na(merged_data$daily.visibility[i])) {
+    if (is.na(merged_data$daily.cloudCover[i]) | 
+        is.na(merged_data$daily.visibility[i]) | 
+        is.na(merged_data$daily.pressure[i])) {
       records <- merged_data[merged_data$Date == merged_data$Date[i] & 
                                merged_data$daily.icon == merged_data$daily.icon[i] & 
                                merged_data$home.team != merged_data$home.team[i],]
@@ -98,6 +100,8 @@ replace_na <- function(merged_data) {
           merged_data$daily.cloudCover[i] = record$daily.cloudCover
         if (is.na(merged_data$daily.visibility[i]) & !is.na(record$daily.visibility))
           merged_data$daily.visibility[i] <- record$daily.visibility
+        if (is.na(merged_data$daily.pressure[i]) & !is.na(record$daily.pressure))
+          merged_data$daily.pressure[i] <- record$daily.pressure
       }
     }
   }
@@ -108,15 +112,73 @@ merged_data_clean <- replace_na(merged_data)
 merged_data_na <- apply(is.na(merged_data_clean), 2, sum) #Check which attributes have NAs. 
 merged_data_na 
 
-## 4. daily.precipAccumulation
+#Still 2, 14, 14 records are NA. This is because there are no other match records on this day that have matching criteria
+#To handle these NAs, take an average of the attribute for the location, with same daily.icon
+library(dplyr)
+replace_na_v2 <- function(merged_data_clean) {
+  for (i in 1:dim(merged_data_clean)[1]) {
+    if (is.na(merged_data_clean$daily.cloudCover[i]) | 
+        is.na(merged_data_clean$daily.visibility[i]) | 
+        is.na(merged_data_clean$daily.pressure[i])) {
+      avgs <- merged_data_clean %>% 
+        filter(daily.icon == merged_data_clean$daily.icon[i], 
+               home.team == merged_data_clean$home.team[i], 
+               Date != merged_data_clean$Date[i]) %>%
+        select(daily.cloudCover, daily.visibility, daily.pressure) %>% 
+        summarize(avg_cloudcover = mean(daily.cloudCover, na.rm = TRUE), 
+                  avg_visiblity = mean(daily.visibility, na.rm = TRUE), 
+                  avg_pressure = mean(daily.pressure, na.rm = TRUE))
+      if (is.na(merged_data_clean$daily.cloudCover[i]))
+        merged_data_clean$daily.cloudCover[i] <- avgs$avg_cloudcover
+      if (is.na(merged_data_clean$daily.visibility[i]))
+        merged_data_clean$daily.visibility[i] <- avgs$avg_visiblity
+      if (is.na(merged_data_clean$daily.pressure[i]))
+        merged_data_clean$daily.pressure[i] <- avgs$avg_pressure
+      }
+    }
+  return(merged_data_clean)
+}
+
+merged_data_clean <- replace_na_v2(merged_data_clean)
+merged_data_na <- apply(is.na(merged_data_clean), 2, sum) #Check which attributes have NAs. cloudcover 8, visibility 2
+merged_data_na 
+
+#Remove the two row that have NA in cloudcover and visibility
+colnames(merged_data_clean)[apply(merged_data_clean, 2, anyNA)]
+merged_data_clean <- merged_data_clean[complete.cases(merged_data_clean), ]
+merged_data_na <- apply(is.na(merged_data_clean), 2, sum) #Check which attributes have NAs. cloudcover 8, visibility 2
+merged_data_na 
+
+## d. daily.precipAccumulation
 sum(is.na(merged_data$daily.precipAccumulation)) #3375 NAs
 #Almost all rows have NA. Remove this attribute 
-merged_data_clean <- within(merged_data, rm(daily.precipAccumulation))
+merged_data_clean <- within(merged_data_clean, rm(daily.precipAccumulation))
 str(merged_data_clean)
- 
+
+
+# 5. Perform feature selection (removing attributes) and feature extraction (deriving new attributes)
+str(merged_data_clean)
+#Removing "Referee, FTHG, FTAG, id, club, location, coordinates, Latitude, Longitude, daily.sunrisesTime, daily.sunsetTime,
+#daily.precipIntensityMaxTime, daily.temperatureHighTime, daily.temperatureLowTime,id, daily.apparentTemperatureHighTime,
+#daily.apparentTemperatureLowTime, daily.windGustTime, daily.uvIndexTime, daily.temperatureMinTime, daily.temperatureMaxTime,
+#daily.apparentTemperatureMinTime, daily.apparentTemperatureMaxTime, location, home.team
+#drop columns using dplyr function drop()
+final_data <- select (merged_data_clean,-c(Referee, FTHG, FTAG, Id, Club, Location, Coordinates, Latitude, Longitude, 
+                                           daily.sunriseTime, daily.sunsetTime, daily.precipIntensityMaxTime, 
+                                           daily.temperatureHighTime, daily.temperatureLowTime,id, 
+                                           daily.apparentTemperatureHighTime,daily.apparentTemperatureLowTime, 
+                                           daily.windGustTime, daily.uvIndexTime, daily.temperatureMinTime, 
+                                           daily.temperatureMaxTime,daily.apparentTemperatureMinTime, 
+                                           daily.apparentTemperatureMaxTime, location, home.team))
+str(final_data)
+
+
 #Outlier detection
 
-# 5. Perform feature selection
 
 # 6. Perform exploratory analysis
-   
+str(merged_data_clean)
+boxplot(merged_data_clean$HTHG)$out
+hist(merged_data_clean$HTHG)
+boxplot(merged_data_clean$HTAG)$out
+hist(merged_data_clean$HTAG)
