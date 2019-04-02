dd82d224bcdb3cf8308ea0631f9840c434d1450c
shradha berry <shradhaberry@gmail.com>
2019-04-02 17:01:56 +0100

    Supervised and performance evaluation

--- scripts/4.Supervised.R
+++ scripts/4.Supervised.R
@@ -43,15 +43,19 @@ library(randomForest)
 #data <- as.data.frame(unclass(data));
 # 5. Random forest training
 
-floor(sqrt(ncol(training_data[, -c(19, 22, 23, 24, 25)])))
+library(caret)
+ctrl_parameters <- trainControl(method = 'CV', number = 10)
 
 # train a model with random forest
 #   note: number of trees is set to 500
 #     and calculation of attributes importance is requested
-rf <- randomForest(formula, ntree = 500, importance = T, data = training_data)
-rf1 <- randomForest(formula, ntree = 800, mtry = 6, importance = T, data = training_data)
-rf2 <- randomForest(formula, ntree = 2000, importance = T, data = training_data)
-
+rf <- randomForest(formula, ntree = 500, importance = T, data = training_data) ##plot shows 300 trees ideal
+rf1 <- randomForest(formula, ntree = 500, mtry = 6, importance = T, data = training_data)
+rf2 <- randomForest(formula, ntree = 500, mtry = 8, importance = T, data = training_data)
+rf3 <- randomForest(formula, ntree = 500, mtry = 12, importance = T, data = training_data)
+rf4 <- randomForest(formula, ntree = 500, mtry = 15, importance = T, data = training_data)
+rf5 <- randomForest(formula, ntree = 500, mtry = 20, importance = T, data = training_data)
+rf6 <- train(formula, data = training_data, method = "rf", trControl = ctrl_parameters)
 # plot the error rates
 #   note: the labels for the legend are extracted from the rf object
 #     and they include Out-of-bag (OOB) error. OOB is the average error
@@ -60,12 +64,19 @@ rf2 <- randomForest(formula, ntree = 2000, importance = T, data = training_data)
 plot(rf)
 plot(rf1)
 plot(rf2)
+plot(rf3)
+plot(rf4)
+plot(rf5)
+plot(rf6)
 legend('topright', colnames(rf$err.rate), bty = 'n', lty = c(1,2,3), col = c(1:3))
 
 # plot the variable importancea according to the
 varImpPlot(rf, type = 1)
 varImpPlot(rf1, type = 1)
 varImpPlot(rf2, type = 1)
+varImpPlot(rf3, type = 1)
+varImpPlot(rf4, type = 1)
+varImpPlot(rf5, type = 1)
 
 # 6. Random forest prediction
 
@@ -74,6 +85,10 @@ varImpPlot(rf2, type = 1)
 rf_pred <- predict(rf, test_data[,-22], type= "class")
 rf_pred1 <- predict(rf1, test_data[,-22], type= "class")
 rf_pred2 <- predict(rf2, test_data[,-22], type= "class")
+rf_pred3 <- predict(rf3, test_data[,-22], type= "class")
+rf_pred4 <- predict(rf4, test_data[,-22], type= "class")
+rf_pred5 <- predict(rf5, test_data[,-22], type= "class")
+rf_pred6 <- predict(rf6, test_data[,-22], type= "raw")
 
 # create a contingency table for the actual VS predicted for the random forest model
 rf_results_table <- table(rf = rf_pred,  actual = test_data$FTR)
@@ -82,15 +97,31 @@ rf_results_table1 <- table(rf = rf_pred1,  actual = test_data$FTR)
 rf_results_table1
 rf_results_table2 <- table(rf = rf_pred2,  actual = test_data$FTR)
 rf_results_table2
+rf_results_table3 <- table(rf = rf_pred3,  actual = test_data$FTR)
+rf_results_table3
+rf_results_table4 <- table(rf = rf_pred4,  actual = test_data$FTR)
+rf_results_table4
+rf_results_table5 <- table(rf = rf_pred5,  actual = test_data$FTR)
+rf_results_table5
+rf_results_table6 <- table(rf = rf_pred6,  actual = test_data$FTR)
+rf_results_table6
 
 # calculate accuracy from each contigency table
 #   as sum of diagonal elements over sum of the matrix values
-acc_rf <- sum(diag(rf_results_table)) / sum(rf_results_table)
+acc_rf <- sum(diag(rf_results_table)) / sum(rf_results_table) #0.6296296
 acc_rf
-acc_rf1 <- sum(diag(rf_results_table1)) / sum(rf_results_table1)
+acc_rf1 <- sum(diag(rf_results_table1)) / sum(rf_results_table1) #0.6393762
 acc_rf1
-acc_rf2 <- sum(diag(rf_results_table1)) / sum(rf_results_table1)
+acc_rf2 <- sum(diag(rf_results_table2)) / sum(rf_results_table2) #0.6218324
 acc_rf2
+acc_rf3 <- sum(diag(rf_results_table3)) / sum(rf_results_table3) #0.6237817
+acc_rf3
+acc_rf4 <- sum(diag(rf_results_table4)) / sum(rf_results_table4) #0.6345029
+acc_rf4
+acc_rf5 <- sum(diag(rf_results_table5)) / sum(rf_results_table5) #0.6218324
+acc_rf5
+acc_rf6 <- sum(diag(rf_results_table6)) / sum(rf_results_table6) #0.6218324
+acc_rf6
 
 #########################################################################
 
@@ -99,32 +130,34 @@ library(neuralnet)
 # 3. Neural network training 
 #IMPORTANT: Error closer to zero is better. Three architectures are used below to see which is giving lower error.
 
-idx <- sapply(data, is.factor)
-data[idx] <- lapply(data[idx], function(x) as.numeric(x))
+data_nn <- data[, -c(19, 23, 24, 25)]
+idx <- sapply(data_nn, is.factor)
+data_nn[idx] <- lapply(data_nn[idx], function(x) as.numeric(x))
 #data["FTR"] <- PCA_full$FTR
+str(data_nn)
 
 # set random seed
 set.seed(2018)
 # create a 70/30 training/test set split
-n_rows <- nrow(data)
+n_rows <- nrow(data_nn)
 # sample 70% (n_rows * 0.7) indices in the ranges 1:nrows
 training_idx <- sample(n_rows, n_rows * 0.7)
 # filter the data frame with the training indices (and the complement)
-training_data <- data[training_idx,]
-test_data <- data[-training_idx,]
-formula <- reformulate(names(training_data[, -c(19, 22, 23, 24, 25)]), response = 'FTR')
+nn_training_data <- data_nn[training_idx,]
+nn_test_data <- data_nn[-training_idx,]
+nn_formula <- reformulate(names(nn_training_data[, -c(21)]), response = 'FTR')
 
 # train a neural network with 1 hidden node
-nn_1 <- neuralnet(formula, data = training_data)
+nn_1 <- neuralnet(nn_formula, data = nn_training_data)
 nn_1$result.matrix
 
 # train a neural network with 5 nodes on one hidden layer
 #   note: the number of layers is set with the hidden option parameter
-nn_5 <- neuralnet(formula, hidden = 5, data = training_data)
+nn_5 <- neuralnet(nn_formula, hidden = 5, data = nn_training_data)
 nn_5$result.matrix
 
 # train a neural network with 5 nodes on each of two hidden layers
-nn_55 <- neuralnet(formula, hidden = c(5,5), data = training_data, stepmax = 2000000)
+nn_55 <- neuralnet(nn_formula, hidden = c(5,5), data = nn_training_data, stepmax = 2000000)
 nn_55$result.matrix
 
 # plot the three neural networks and compare their structure
@@ -137,13 +170,9 @@ plot(nn_55)
 
 # compute the prediction for each neural network
 #   note: the strength attribute (column 9) is excluded from the test data set
-pred_nn_1 <- compute(nn_1, test_data[, -c(19, 22, 23, 24, 25)])
-pred_nn_5 <- compute(nn_5, test_data[, -c(19, 22, 23, 24, 25)])
-pred_nn_55 <- compute(nn_55, test_data[, -c(19, 22, 23, 24, 25)])
-
-nn_57 <- neuralnet(formula, hidden = c(5,7), data = training_data)
-plot(nn_57)
-pred_nn_57 <- compute(nn_57, test_data[,-22])
+pred_nn_1 <- compute(nn_1, nn_test_data[, -c(21)])
+pred_nn_5 <- compute(nn_5, nn_test_data[, -c(21)])
+pred_nn_55 <- compute(nn_55, nn_test_data[, -c(21)])
 
 # create a table with actual values and the three predictions
 #   note: predicted values are stored as net_result attribute of the prediction object
@@ -151,8 +180,7 @@ nn_results <- data.frame(
   actual = test_data$FTR,
   nn_1 = pred_nn_1$net.result,
   nn_5 = pred_nn_5$net.result,
-  nn_55 = pred_nn_55$net.result,
-  nn_57 = pred_nn_57$net.result
+  nn_55 = pred_nn_55$net.result
 )
 
 # calculate the correlation between actual and predicted values to identify the best predictor
--- scripts/5.Performance_Evaluation.R
+++ scripts/5.Performance_Evaluation.R
@@ -1,51 +1,63 @@
+
 ######################################################################
 #   6. Performance evaluation
 
-tchurn_rf_predict <-  cbind(
-  actual = test_data$FTR,
-  predicted = predict(rf, test_data[,-22], type = 'raw'),
-  predict(rf, test_data[,-22], type = 'prob')
-)
+library(caret)
 
 # generate a confusion matrix for the each predicted model
 #   and inspect them: the caret confusionMatrix function
 #   returns also Accuracy, Kappa, Sensitivity and Specificity
 #     note: the positive class should be explicitely declared
 #       with the argument 'positive'
-rf_confmat <- confusionMatrix(data = rf_predict$predicted, reference = rf_predict$actual, positive = "True")
-rf_confmat <- confusionMatrix(data = tchurn_rf_predict$predicted, reference = tchurn_rf_predict$actua, positive = "True")
-tree_confmat
-rf_confmat
+rf_confmat <- confusionMatrix(data = rf_pred1, reference = test_data$FTR, positive = "True")
+nn_confmat <- confusionMatrix(data = as.factor(round(pred_nn_1$net.result, 0)), reference = as.factor(nn_test_data$FTR), 
+                              positive = "True")
+nn_confmat
+prob.result <- pred_nn_1$net.result
+
+#install.packages("pROC")
+library(pROC)
+multi_roc <- multiclass.roc(test_data$FTR, predict(rf1, test_data[, -22], type = 'prob'))
+auc(multi_roc)
+
+rf_predicted <-  cbind(
+  actual = test_data$FTR,
+  predicted = rf_pred1,
+  predict(rf1, test_data[, -22], type = 'prob')
+)
+rf_predicted <- as.data.frame(rf_predicted)
 
 # prepare two data frames to generate a ROC curve:
 #   a data frame with the probability scores for the prediction of True
 #   a data frame with the actual classes (repeated twice)
-tchurn_models_prob <- data.frame(
-  tree = tchurn_tree_predict$True,
-  rf = tchurn_rf_predict$True
+models_prob <- data.frame(
+  rf1 = rf_predicted$H,
+  nn = prob.result
 )
-tchurn_label <- data.frame(
-  tree = tchurn_tree_predict$actual,
-  rf = tchurn_rf_predict$actual
+levels(test_data$FTR) <- list(H=c("H"), AorD=c("A", "D"))
+
+label <- data.frame(
+  rf1 = test_data$FTR,
+  nn1 = test_data$FTR
 )
 
 # ROCR requires to create a prediction and a performance object
 #   note: the performance object can be created for different measures
 #     e.g. TPR and FPR in this case
-tchurn_ROC_pred = prediction(tchurn_models_prob, tchurn_label)
-tchurn_ROC_perf = performance(tchurn_ROC_pred, "tpr", "fpr")
+ROC_pred = prediction(models_prob, label)
+ROC_perf = performance(ROC_pred, "tpr", "fpr")
 
 # plot the ROC curve for the two methods
 opar <- par()
 par(pty = 's')
 plot(
-  tchurn_ROC_perf,
+  ROC_perf,
   col = as.list(c("orange", "blue"))
 )
 abline(a = 0, b = 1, lty = 2, col = 'red')
 legend(
   "bottomright",
-  names(tchurn_models_prob),
+  names(models_prob),
   col = c("orange", "blue"),
   lty = 1,
   bty = 'n'
